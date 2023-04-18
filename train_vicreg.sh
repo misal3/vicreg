cb0ae392b566aca5e8eeefe25279909c18cd5e73
michael.s <michael.salm@students.fhnw.ch>
2023-04-18 22:08:38 +0200

    adopt sbatch file to accomodate single threaded vicreg

--- train_vicreg.sh
+++ train_vicreg.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-echo $MASTER_ADDR
-echo $MASTER_PORT
+echo "vicrec"
 #SBATCH -p performance
-#SBATCH -w node15
-#SBATCH --exclude=node22
-#SBATCH --gpus=4
 #SBATCH -t 1-02:03:04
 #SBATCH --job-name=vicreg
 #SBATCH --out=out.log
 #SBATCH --err=err.log
-python3 /home2/michael.s/vicreg/main_vicreg.py --data-dir /home2/michael.s/imagenet/ILSVRC/Data/CLS-LOC --exp-dir /home2/michael.s/vicreg_experiment --dist-url 'tcp://10.35.146.96:9988' --num-workers 8 --world-size 4
+echo $MASTER_ADDR
+echo $MASTER_PORT
+python3 /home2/michael.s/vicreg/main_vicreg2.py --data-dir /home2/michael.s/imagenet/ILSVRC/Data/CLS-LOC --exp-dir /home2/michael.s/vicreg_experiment --dist-url 'tcp://10.35.146.45:32000' --num-workers 8 --world-size 4 --batch-size 32
