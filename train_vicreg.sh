249724ae320ca50c6a40dddba71740660d6e53a5
michael.s <michael.salm@students.fhnw.ch>
2023-04-23 13:37:12 +0200

    clean up script

--- train_vicreg.sh
+++ train_vicreg.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "vicrec"
 #SBATCH -p performance
+#SBATCH --exclude node22
 #SBATCH -t 1-02:03:04
 #SBATCH --job-name=vicreg
 #SBATCH --out=out.log
 #SBATCH --err=err.log
-echo $MASTER_ADDR
-echo $MASTER_PORT
-python3 /home2/michael.s/vicreg/main_vicreg2.py --data-dir /home2/michael.s/imagenet/ILSVRC/Data/CLS-LOC --exp-dir /home2/michael.s/vicreg_experiment --dist-url 'tcp://10.35.146.45:32000' --num-workers 8 --world-size 4 --batch-size 32
+python3 /home2/michael.s/vicreg/main_vicreg2.py --data-dir /home2/michael.s/imagenet/ILSVRC/Data/CLS-LOC --exp-dir /home2/michael.s/vicreg_experiment --num-workers 1 --world-size 1 --batch-size 32 --data-subset-size 10000
