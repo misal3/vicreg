4a9322d345f76e2d46e922f7c2d06f1eea3776b7
michael.s <michael.salm@students.fhnw.ch>
2023-05-13 21:07:13 +0200

    minor changes

--- train_vicreg.sh
+++ train_vicreg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -p performance
 #SBATCH -w node14
-#SBATCH --gpus 3
-#SBATCH -t 1-02:03:04
-#SBATCH --job-name=vicreg
-#SBATCH --out=out.log
-#SBATCH --err=err.log
-python3 /home2/michael.s/vicreg/main_vicreg2.py --data-dir /home2/michael.s/imagenet/ILSVRC/Data/CLS-LOC --exp-dir /home2/michael.s/vicreg/vicreg_experiment --num-workers 1 --world-size 1 --batch-size 64 --data-subset-size 10000
+#SBATCH --gpus=1
+#SBATCH -t 2-02:03:04
+#SBATCH --job-name=vicreg_1
+#SBATCH --out=out-1.log
+#SBATCH --err=err-1.log
+python3 /home2/michael.s/vicreg/main_vicreg2.py --data-dir /home2/michael.s/imagenet/ILSVRC/Data/CLS-LOC --exp-dir /home2/michael.s/vicreg/vicreg_experiment/randomsubset_10000_bs32/ --num-workers 1 --world-size 1 --batch-size 32 --data-subset-size 10000
