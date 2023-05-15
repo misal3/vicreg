d89b51023b7ddd79a6e3527ceca2c94a324ebd01
michael.s <michael.salm@students.fhnw.ch>
2023-05-15 12:12:48 +0200

    change to make sure subfolder for plots exists

--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from pathlib import Path
 import seaborn as sns
+import os
 
 
 def save_heatmap(tensor: torch, downsampling: int, plot_title: str, filepath: Path) -> None:
@@ -13,5 +14,12 @@ def save_heatmap(tensor: torch, downsampling: int, plot_title: str, filepath: Pa
     logging.info(f'downsampled_tensor: {downsampled_tensor}')
     sns.heatmap(downsampled_tensor, cmap='viridis')
     plt.title(plot_title)
+    directory = os.path.dirname(filepath)
+    os.makedirs(directory, exist_ok=True)
     plt.savefig(filepath)
     plt.close()
+
+
+def downsample_cov(tensor: torch, downsampling: int) ->torch:
+    tensor_cpu = tensor.detach().clone().cpu()
+    return tensor_cpu[::downsampling, ::downsampling]
